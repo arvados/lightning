d5fffab2d3d809250fb10492851cff17fe9c460d
Tom Clegg <tom@tomclegg.ca>
2020-10-21 23:45:06 -0400

    Update test.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@tomclegg.ca>

--- pipeline_test.go
+++ pipeline_test.go
@@ -88,17 +88,19 @@ func (s *pipelineSuite) TestImportMerge(c *check.C) {
 	c.Check(code, check.Equals, 0)
 	c.Check(hgvsout.Len() > 0, check.Equals, true)
 	c.Logf("%s", hgvsout.String())
-	c.Check(hgvsout.String(), check.Equals, `chr1:g.1_3delinsGGC
-chr1:g.[41_42delinsAA];[41=]
-chr1:g.[161=];[161A>T]
-chr1:g.[178=];[178A>T]
-chr1:g.222_224del
-chr1:g.[302=];[302_305delinsAAAA]
-chr2:g.[813_826del];[813=]
-chr2:g.[830_841delinsAA];[830=]
-chr2:g.[887C>A];[887=]
-chr2:g.[1042_1044del];[1042=]
-chr2:g.[1043=];[1043_1044delinsAA]
+	c.Check(hgvsout.String(), check.Equals, `chr1:g.1_3delinsGGC	.
+chr1:g.[41_42delinsAA];[41=]	.
+chr1:g.[161=];[161A>T]	.
+chr1:g.[178=];[178A>T]	.
+chr1:g.222_224del	.
+chr1:g.[302=];[302_305delinsAAAA]	.
+.	chr2:g.[1=];[1_3delinsAAA]
+.	chr2:g.125_127delinsAAA
+chr2:g.[241_254del];[241=]	.
+chr2:g.[258_269delinsAA];[258=]	.
+chr2:g.[315C>A];[315=]	.
+chr2:g.[470_472del];[470=]	.
+chr2:g.[471=];[471_472delinsAA]	.
 `)
 
 	vcfout := &bytes.Buffer{}
@@ -106,28 +108,30 @@ chr2:g.[1043=];[1043_1044delinsAA]
 	c.Check(code, check.Equals, 0)
 	c.Check(vcfout.Len() > 0, check.Equals, true)
 	c.Logf("%s", vcfout.String())
-	c.Check(vcfout.String(), check.Equals, `chr1	1	NNN	GGC	1/1
-chr1	41	TT	AA	1/0
-chr1	161	A	T	0/1
-chr1	178	A	T	0/1
-chr1	221	TCCA	T	1/1
-chr1	302	TTTT	AAAA	0/1
-chr2	812	ATTTTTCTTGCTCTC	A	1/0
-chr2	830	CCTTGTATTTTT	AA	1/0
-chr2	887	C	A	1/0
-chr2	1041	GTGG	G	1/0
-chr2	1043	GG	AA	0/1
+	c.Check(vcfout.String(), check.Equals, `chr1	1	NNN	GGC	1/1	0/0
+chr1	41	TT	AA	1/0	0/0
+chr1	161	A	T	0/1	0/0
+chr1	178	A	T	0/1	0/0
+chr1	221	TCCA	T	1/1	0/0
+chr1	302	TTTT	AAAA	0/1	0/0
+chr2	1	TTT	AAA	0/0	0/1
+chr2	125	CTT	AAA	0/0	1/1
+chr2	240	ATTTTTCTTGCTCTC	A	1/0	0/0
+chr2	258	CCTTGTATTTTT	AA	1/0	0/0
+chr2	315	C	A	1/0	0/0
+chr2	469	GTGG	G	1/0	0/0
+chr2	471	GG	AA	0/1	0/0
 `)
 	bedout, err := ioutil.ReadFile(tmpdir + "/export.bed")
 	c.Check(err, check.IsNil)
 	c.Logf("%s", string(bedout))
-	c.Check(string(bedout), check.Equals, `chr1 0 248 0 1000 . 0 224
-chr1 224 372 1 500 . 248 348
+	c.Check(string(bedout), check.Equals, `chr1 0 248 0 500 . 0 224
+chr1 224 372 1 250 . 248 348
 chr1 348 496 2 0 . 372 472
 chr1 472 572 3 0 . 496 572
-chr2 572 820 4 500 . 0 796
-chr2 796 944 5 0 . 820 920
-chr2 920 1068 6 1000 . 944 1044
-chr2 1044 1144 7 0 . 1068 1144
+chr2 0 248 4 750 . 0 224
+chr2 224 372 5 0 . 248 348
+chr2 348 496 6 500 . 372 472
+chr2 472 572 7 0 . 496 572
 `)
 }
