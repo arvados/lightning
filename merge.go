82d7552a13768defa1c2c007d988e8174221415c
Tom Clegg <tom@tomclegg.ca>
2020-12-15 09:31:30 -0500

    Bump merge memory.
    Arvados-DCO-1.1-Signed-off-by: Tom Clegg <tom@curii.com>

--- merge.go
+++ merge.go
@@ -70,10 +70,11 @@ func (cmd *merger) RunCommand(prog string, args []string, stdin io.Reader, stdou
 			Name:        "lightning merge",
 			Client:      arvados.NewClientFromEnv(),
 			ProjectUUID: *projectUUID,
-			RAM:         150000000000,
+			RAM:         250000000000,
 			VCPUs:       16,
 			Priority:    *priority,
 			APIAccess:   true,
+			KeepCache:   1,
 		}
 		for i := range cmd.inputs {
 			err = runner.TranslatePaths(&cmd.inputs[i])
